3140eb0b8272a6dfc2013364cf3c7d85ec5345af
Ryan Phillips <wordsandcolor@gmail.com>
2014-04-23 13:00:20 -0700

    i guess I'll call this finished

--- hw3/a3q3.m
+++ hw3/a3q3.m
@@ -1,7 +1,8 @@
 % a3q3.m
 % Code for running Jacobi and G-S iterations for Question 3 on Assignment 3
 
-A = [9 -9 9; -9 10 -10; 9 -10 14]; b = A*[1;1;1];
+% A = [9 -9 9; -9 10 -10; 9 -10 14]; b = A*[1;1;1];
+A = [300 1 1; 2 5 2; 3 3 7]; b = A*[1;1;1];
 format long
 
 %N = diag(diag(A)); P = N-A;    %Jacobi iteration -- N is the diagonal part of A
